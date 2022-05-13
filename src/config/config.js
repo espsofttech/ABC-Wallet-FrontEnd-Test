ede860604ec9f736cd216b0b3370b0c739d23a94
unknown <rahuliitm69@gmail.com>
2022-05-13 13:22:01 +0530

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -2,24 +2,24 @@ const config = {
 
     //=========================  Localhost ========================
 
-    // baseUrl: '/abc-wallet/',
-    // apiUrl: "http://localhost:9000/api/",
+    // baseUrl: '/',
+    // apiUrl: "https://espsofttech.tech:6017/api/",
     // imageUrl: "https://espsofttech.tech/abc-wallet/backend/uploads/",
     // // loaderType:"Ball-Triangle"
 
     //=========================  Esp server ========================
 
-    // baseUrl: '/abc-wallet/',
-    // apiUrl: "https://espsofttech.tech:6017/api/",
-    // imageUrl:"https://espsofttech.tech/abc-wallet/backend/uploads/", 
-    // loaderType:"Ball-Triangle"
+    baseUrl: '/abc-wallet/',
+    apiUrl: "https://espsofttech.tech:6017/api/",
+    imageUrl:"https://espsofttech.tech/abc-wallet/backend/uploads/", 
+    loaderType:"Ball-Triangle"
 
     //=========================  Live server ========================
 
-    baseUrl: '/',
-    apiUrl: "http://18.217.124.116:9000/api/",
-    imageUrl: "https://18.217.124.116/backend/uploads/",
-    loaderType:"Ball-Triangle"
+    // baseUrl: '/',
+    // apiUrl: "http://18.217.124.116:9000/api/",
+    // imageUrl: "https://18.217.124.116/backend/uploads/",
+    // loaderType:"Ball-Triangle"
 
     // baseUrl: '/',
     // apiUrl: "http://18.217.124.116:9000/api/",
